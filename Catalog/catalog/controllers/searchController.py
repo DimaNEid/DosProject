90915a8a9979a9f1f6f799305c77942a81dbdad4
DimaNEid <dima.eid11@gmail.com>
2024-10-29 19:41:13 +0200

    notUsedimports

--- Catalog/catalog/controllers/searchController.py
+++ Catalog/catalog/controllers/searchController.py
@@ -1,30 +1,3 @@
-# from rest_framework import viewsets
-# from rest_framework.decorators import action
-# from rest_framework.response import Response
-# from catalog.method.search import search_books
-#
-# class searchController(viewsets.ViewSet):
-#     def list(self, request):
-#         topic = request.query_params.get('topic', None)
-#         if not topic:
-#             return Response({"error": "Topic parameter is required"}, status=400)
-#         result = search_books(topic)
-#         return Response(result)
-#
-# from rest_framework import viewsets
-# from rest_framework.response import Response
-# from catalog.method.search import search_books
-#
-#
-# class searchController(viewsets.ViewSet):
-#     def list(self, request):
-#         print("Search endpoint reached")  # Debugging line
-#         topic = request.query_params.get('topic', None)
-#         if not topic:
-#             return Response(data={"error": "Topic parameter is required"}, status=400)
-#
-#         result = search_books(topic)
-#         return Response(result)
 
 from rest_framework import viewsets
 from rest_framework.response import Response
--- Catalog/catalog/controllers/updateController.py
+++ Catalog/catalog/controllers/updateController.py
@@ -1,6 +1,5 @@
 
 from rest_framework import viewsets
-from rest_framework.decorators import action
 from rest_framework.response import Response
 from catalog.method.update import update_book_info
 
