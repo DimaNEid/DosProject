26f84fccfd3a4f24c79e7c99e802e723d9e2edcc
DimaNEid <dima.eid11@gmail.com>
2024-10-29 19:21:50 +0200

    order_PurchaseController_finalSetUp_removing_unwanted_comments

--- Order/order/controllers/PurchaseController.py
+++ Order/order/controllers/PurchaseController.py
@@ -1,43 +1,9 @@
-# from django.db.models.expressions import result
-# from rest_framework import viewsets, status
-# from rest_framework.response import Response
-#
-# from order.method.order import handle_purchase
-#
-#
-# class orderController(viewsets.ViewSet):
-#     def update(self, request, pk=None):
-#         quantity = request.data['quantity',None]
-#
-#         if quantity is None:
-#             return Response({'error':'Quantity parameter is required'}, status=status.HTTP_400_BAD_REQUEST)
-#
-#         result, status_code = handle_purchase(pk, quantity)
-#         return Response(result, status=status_code)
-
-
-# class purchaseController(viewsets.ViewSet):
-#     def update(self, request, pk=None):
-#         """Handle the purchase process using primary key (pk)."""
-#         # Using .get to safely access 'count' from request data
-#         count = request.data.get('count', None)
-#
-#         if not count:
-#             return Response({"error": "Count parameter is required"}, status=400)
-#
-#         # Call the purchase handling method
-#         result, status_code = handle_purchase(pk, int(count))
-#         return Response(result, status=status_code)
-
 
 from rest_framework import viewsets
 from rest_framework.response import Response
-
 from order.method.purchase import purchase_book
 
-
 class PurchaseController(viewsets.ViewSet):
-
 
     def update(self, request, pk=None):
 
