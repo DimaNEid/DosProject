1692ccc876b5a22d30681aa0e72a69893a76ed22
DimaNEid <dima.eid11@gmail.com>
2024-10-29 19:33:54 +0200

    edit_services_urls

--- FE/fe/services_urls_config.py
+++ FE/fe/services_urls_config.py
@@ -1,16 +1,5 @@
-# # urls_config.py
-#
-# # Base URLs for external services
-# BASE_URL_CATALOG = "http://127.0.0.1:8000/catalog"
-# BASE_URL_ORDER = "http://127.0.0.1:8002/order"
-#
-# # Endpoint URLs for specific paths
-# URL_CATALOG_INFO = f"{BASE_URL_CATALOG}/info/"
-# URL_CATALOG_SEARCH = f"{BASE_URL_CATALOG}/search/"
-# URL_ORDER_PURCHASE = f"{BASE_URL_ORDER}/purchase/"
 
-# services_urls_config.py in routes folder
 
-URL_CATALOG_SEARCH = "http://127.0.0.1:8000/catalog/search"
-URL_CATALOG_INFO = "http://127.0.0.1:8000/catalog/info/"
-URL_ORDER_PURCHASE = "http://127.0.0.1:8002/order/purchase/"
+URL_CATALOG_SEARCH = "http://127.0.0.1:8000/catalog_service/search"
+URL_CATALOG_INFO = "http://127.0.0.1:8000/catalog_service/info/"
+URL_ORDER_PURCHASE = "http://127.0.0.1:8002/order_service/purchase/"
