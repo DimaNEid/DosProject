9051265ad38c656079ad3b398298fa6876a2d1f2
DimaNEid <dima.eid11@gmail.com>
2024-10-29 19:36:13 +0200

    edit_controller_from@action_to_retrieve

--- FE/fe/controllers/InfoController.py
+++ FE/fe/controllers/InfoController.py
@@ -9,15 +9,3 @@ class InfoController(viewsets.ViewSet):
         return Response(info_data, status=status)
 
 
-
-# from rest_framework import viewsets
-# from rest_framework.response import Response
-# from fe.method.info import get_info
-#
-# class InfoController(viewsets.ViewSet):
-#     def list(self, request):
-#         """Handles the request to get info by delegating to the method file."""
-#         info_data, status = get_info()
-#         return Response(info_data, status=status)
-
-import requests
--- FE/fe/method/info.py
+++ FE/fe/method/info.py
@@ -5,15 +5,5 @@ def get_info(pk):
     response = requests.get(URL_CATALOG_INFO + str(pk))
     if response.status_code == 200:
         return response.json(), response.status_code
-    return {"error": "Failed to fetch info from Catalog service"}, response.status_code
-
-# import requests
-#
-# def get_info():
-#     """Fetches information from the Catalog service."""
-#     catalog_url = "http://127.0.0.1:8000/catalog/info/"
-#     response = requests.get(catalog_url)
-#     if response.status_code == 200:
-#         return response.json()
-#     return {"error": "Failed to fetch info from Catalog service"}, response.status_code
+    return {"error": "Failed to fetch book info from Catalog service"}, response.status_code
 
