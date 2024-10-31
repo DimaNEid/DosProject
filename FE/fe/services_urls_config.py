7b25d1f68a21ac7b8dadfc0a7013173a131bfe70
DimaNEid <dima.eid11@gmail.com>
2024-10-31 13:24:53 +0200

    edit-urls

--- FE/fe/services_urls_config.py
+++ FE/fe/services_urls_config.py
@@ -1,5 +1,10 @@
 
 
-URL_CATALOG_SEARCH = "http://127.0.0.1:8000/catalog_service/search"
-URL_CATALOG_INFO = "http://127.0.0.1:8000/catalog_service/info/"
-URL_ORDER_PURCHASE = "http://127.0.0.1:8002/order_service/purchase/"
+# URL_CATALOG_SEARCH = "http://127.0.0.1:8000/catalog_service/search"
+# URL_CATALOG_INFO = "http://127.0.0.1:8000/catalog_service/info/"
+# URL_ORDER_PURCHASE = "http://127.0.0.1:8002/order_service/purchase/"
+
+URL_CATALOG_SEARCH = "http://catalogservice:8000/catalog_service/search"
+URL_CATALOG_INFO = "http://catalogservice:8000/catalog_service/info/"
+URL_ORDER_PURCHASE = "http://orderservice:8002/order_service/purchase/"
+
