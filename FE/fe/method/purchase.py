fa35f256cc08fdcdb5f3e34932db17feab75a782
DimaNEid <dima.eid11@gmail.com>
2024-10-31 13:23:57 +0200

    return-response-from-order

--- FE/fe/method/purchase.py
+++ FE/fe/method/purchase.py
@@ -26,4 +26,4 @@ def make_purchase(pk, data):
     response = requests.put(f"{URL_ORDER_PURCHASE}{pk}", json=data)
     if response.status_code == 200:
         return response.json(), response.status_code
-    return {"error": "Failed to update purchase in Order service"}, response.status_code
+    return response.json(), response.status_code
