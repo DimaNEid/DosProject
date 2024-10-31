9528430d4395518666eae730a37ff1dd863dc4b4
DimaNEid <dima.eid11@gmail.com>
2024-10-31 13:22:56 +0200

    check-response

--- FE/fe/method/info.py
+++ FE/fe/method/info.py
@@ -5,5 +5,5 @@ def get_info(pk):
     response = requests.get(URL_CATALOG_INFO + str(pk))
     if response.status_code == 200:
         return response.json(), response.status_code
-    return {"error": "Failed to fetch book info from Catalog service"}, response.status_code
+    return {"error": f"Failed to fetch book info from Catalog service, error code {response.status_code}, response: {response.text}"}, response.status_code
 
